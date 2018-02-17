8e6b70929dbcb061464f417251e0d8ae48f89060
Zsolt Takacs <sajtosstangli@gmail.com>
2018-02-17 18:47:02 +0100

    add initial version

---
+++ WpfPleaseWaitExample/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace BusyIndicatorExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public MainWindow()
+        {
+            DataContext = _mainWindowViewModel = new MainWindowViewModel();
+
+            InitializeComponent();
+        }
+
+        private void FillListBox(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+            // The "Work"
+            backgroundWorker.DoWork += (o, eargs) =>
+            {
+                var itemsList = new List<string>();
+
+                for (int i = 0; i < 100; ++i)
+                {
+                    itemsList.Add($"Item {i}");
+
+                    Thread.Sleep(50);
+                }
+
+                Dispatcher.Invoke((Action)(() => 
+                {
+                    _mainWindowViewModel.ItemsList = itemsList;
+                    
+                }));
+            };
+
+            // What to do when work is done
+            backgroundWorker.RunWorkerCompleted += (o, eargs) => 
+            {
+                _busyIndicator.IsBusy = false;
+            };
+
+            // Set indicator as busy
+            _busyIndicator.IsBusy = true;
+
+            // Start worker
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+}
---
+++ WpfPleaseWaitExample/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BusyIndicatorExample
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<string> _itemsList;
+        public List<string> ItemsList
+        {
+            get { return _itemsList; }
+            set
+            {
+                _itemsList = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            ItemsList = new List<string> { "Please click on the button above to fill me with items!" };
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
